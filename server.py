eaac89e7474de8bde2ecde0f4c3163da23f72967
David <david-adeniji@hotmail.co.uk>
2020-03-27 16:47:31 +0000

    Dynamic site map

--- server.py
+++ server.py
@@ -4,18 +4,18 @@ Created on Sun Mar 15 19:39:29 2020
 
 @author: david
 """
-import datetime
+from datetime import datetime
 import pandas as pd
 import os
-
-from flask import Flask, render_template, request, Markup, redirect
+from urllib.parse import urlparse
+from flask import Flask, render_template, make_response, request
 from newsapi import NewsApiClient
 
 # Initialises flask application
 server = Flask(__name__, static_folder='assets')
 
 def get_year():
-    return datetime.datetime.now().year
+    return datetime.now().year
 
 def update_news():
     # Init
@@ -69,9 +69,37 @@ def ads_txt():
 def news():
     return render_template('news.html', newsa=update_news())
 
+@server.route("/sitemap")
+@server.route("/sitemap/")
 @server.route("/sitemap.xml")
 def sitemap():
-    return render_template("sitemap.xml")
+    """
+        Route to dynamically generate a sitemap of your website/application.
+        lastmod and priority tags omitted on static pages.
+        lastmod included on dynamic content such as blog posts.
+    """
+    host_components = urlparse(request.host_url)
+    host_base = host_components.scheme + "://" + host_components.netloc
+
+    # Static routes with static content
+    static_urls = list()
+    for rule in server.url_map.iter_rules():
+        if not str(rule).startswith("/admin") and not str(rule).startswith("/user"):
+            if "GET" in rule.methods and len(rule.arguments) == 0:
+                url = {
+                    "loc": f"{host_base}{str(rule)}"
+                }
+                static_urls.append(url)
+
+    xml_sitemap = render_template("sitemap_template.xml", static_urls=static_urls, host_base=host_base)
+    response = make_response(xml_sitemap)
+    response.headers["Content-Type"] = "application/xml"
+
+    return response
+        
+# @server.route("/sitemap.xml")
+# def sitemap():
+#     return render_template("sitemap.xml")
 
 # @server.route('/tracker')
 # def tracker():
