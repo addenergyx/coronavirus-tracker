65f6a31595cdf3ffca1e232ee88481add2e9ea42
David <david-adeniji@hotmail.co.uk>
2020-04-10 10:43:41 +0100

    Animate map

--- mapout.py
+++ mapout.py
@@ -14,7 +14,7 @@ import dash_daq as daq
 from dash.dependencies import Input, Output
 import numpy as np
 from components import NationsDropdown, CasesDropdown, Navbar, Footer
-from dataset import get_jhu_dataset, get_recovery_frame, getMarks, clean_data, unix_to_date, getTimeScaleUnix, getTimeScale
+from dataset import get_jhu_dataset, get_recovery_frame, getMarks, clean_data, unix_to_date, getTimeScaleUnix, getTimeScale, get_animation_frame, get_animation_from_postgres
 
 import plotly.express as px
 from newsapi import NewsApiClient
@@ -97,11 +97,11 @@ def generate_html_table(max_rows=10):
 time_scale = ts_confirmed.columns[4:-1]
 time_scale_unix = [int(time.mktime(datetime.datetime.strptime(x, "%m/%d/%y").timetuple())) for x in time_scale]
 
-external_stylesheets =['https://codepen.io/IvanNieto/pen/bRPJyb.css', dbc.themes.BOOTSTRAP]
+external_stylesheets =['https://codepen.io/IvanNieto/pen/bRPJyb.css', dbc.themes.BOOTSTRAP, 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css']
 
 app = dash.Dash(name='map', server=server, url_base_pathname='/map/', external_stylesheets=external_stylesheets, 
                 meta_tags=[
-                    #{ 'name':'viewport','content':'width=device-width, initial-scale=1' },## Fixes media query not showing
+                    { 'name':'viewport','content':'width=device-width, initial-scale=1' },## Fixes media query not showing
                     {
                         'name': 'description',
                         'content': 'This dashboard is designed to monitor events, deaths, and recoveries reported by serveral sources such as WHO and Johns Hopkins University on the n-Cov (Coronavirus). This constantly searches repositories and reviews all countries case studies.'
@@ -171,21 +171,21 @@ body = html.Div(
                         
                         html.Div([CasesDropdown()]),
                         
-                        html.Div([
-                            dcc.Slider(
-                                id='time-frame',
-                                min = getTimeScaleUnix()[0],
-                                max = getTimeScaleUnix()[-1],
-                                value = getTimeScaleUnix()[-1],
-                                #updatemode='drag',
-                                #tooltip = { 'always_visible': True },
-                                marks=getMarks(12),
-                                step=1,
-                            ),
-                        ]),
+                        # html.Div([
+                        #     dcc.Slider(
+                        #         id='time-frame',
+                        #         min = getTimeScaleUnix()[0],
+                        #         max = getTimeScaleUnix()[-1],
+                        #         value = getTimeScaleUnix()[-1],
+                        #         #updatemode='drag',
+                        #         #tooltip = { 'always_visible': True },
+                        #         marks=getMarks(12),
+                        #         step=1,
+                        #     ),
+                        # ]),
                         
                         html.Div([
-                            html.H3("Coronavirus News", style={'color': colors['text'], 'text-align':'center'}),
+                            html.H3("Global Coronavirus News", style={'color': colors['text'], 'text-align':'center'}),
                             generate_html_table()
                         ],style={'padding-bottom':'20px'}),
                         ], style={'padding-right':'20px','padding-left':'20px'}),
@@ -194,19 +194,19 @@ body = html.Div(
               ## Main panel
               dbc.Col(
                   [
-                      dbc.Col([
-                          html.Div([
-                            daq.ToggleSwitch(
-                                id='exclude-china',
-                                value=True,
-                                label=['Excluding China', 'Including China'],
-                                color='#0275d8',
-                                style={
-                                    'color': 'white'
-                                }
-                            ),
-                         ]),
-                      ], width={"size": 2, "offset":9}),
+                      # dbc.Col([
+                      #     html.Div([
+                      #       daq.ToggleSwitch(
+                      #           id='exclude-china',
+                      #           value=True,
+                      #           label=['Excluding China', 'Including China'],
+                      #           color='#0275d8',
+                      #           style={
+                      #               'color': 'white'
+                      #           }
+                      #       ),
+                      #    ]),
+                      # ], width={"size": 2, "offset":9}),
                       
                       html.Div([
                           dcc.Graph(
@@ -215,7 +215,7 @@ body = html.Div(
                             style={'margin' : '0'},
                           ),
                         ]),
-                  ],style={'padding-top':'80px'}
+                  ],style={'padding-top':'80px'}, width=12, lg=9
             )
        ]),
        
@@ -226,7 +226,7 @@ def Homepage():
         Navbar(),
         body,
         Footer()
-    ], style={'backgroundColor': colors['background']})
+    ], style={'backgroundColor': colors['background'], 'overflow-x': 'hidden'})
     return layout
 
 # @app.callback(Output('time-frame','value'), [Input('time-lapse','n_intervals')])
@@ -240,87 +240,93 @@ def Homepage():
 
 
 @app.callback(Output('corona-map', 'figure'), [Input('nation','value'), Input('case','value'), 
-                                               Input('exclude-china','value'), Input('time-frame','value')])
-def update_map(selected_nation, selected_case, click, unix_date):
+                                               #Input('exclude-china','value'), Input('time-frame','value')
+                                              ])
+def update_map(selected_nation, selected_case):
     
     # unix_date=1584071771
     # selected_nation=['Worldwide']
     
+    df = get_animation_from_postgres()
+    
     px.set_mapbox_access_token(mapbox_access_token)
 
-    date = unix_to_date(unix_date)
-    
-    zoom = 3    
+    #date = unix_to_date(unix_date)
+        
+    #zoom = 3    
     
     ## Country Dropdown
     if 'Worldwide' in selected_nation or not selected_nation: 
-        #filtered_df = df
-        filtered_ts_confirmed = ts_confirmed
-        filtered_ts_death = ts_death
-        filtered_ts_recovered = ts_recovered
+        filtered_df = df
+        # filtered_ts_confirmed = ts_confirmed
+        # filtered_ts_death = ts_death
+        # filtered_ts_recovered = ts_recovered
         zoom = 2
     else:
-        #filtered_df = df[df['Country/Region'].isin(selected_nation)] 
-        filtered_ts_confirmed = ts_confirmed[ts_confirmed['Country/Region'].isin(selected_nation)]
-        filtered_ts_death = ts_death[ts_death['Country/Region'].isin(selected_nation)]
-        filtered_ts_recovered = ts_recovered[ts_recovered['Country/Region'].isin(selected_nation)]
+        filtered_df = df[df['country'].isin(selected_nation)] 
+        # filtered_ts_confirmed = ts_confirmed[ts_confirmed['Country/Region'].isin(selected_nation)]
+        # filtered_ts_death = ts_death[ts_death['Country/Region'].isin(selected_nation)]
+        # filtered_ts_recovered = ts_recovered[ts_recovered['Country/Region'].isin(selected_nation)]
     
     ## China Checkbox
-    if not click: 
+    # if not click: 
         #filtered_df = filtered_df[filtered_df['Country/Region'] != 'China'] 
-        filtered_ts_confirmed = filtered_ts_confirmed[filtered_ts_confirmed['Country/Region'] != 'China']
-        filtered_ts_death = filtered_ts_death[filtered_ts_death['Country/Region'] != 'China']
-        filtered_ts_recovered = filtered_ts_recovered[filtered_ts_recovered['Country/Region'] != 'China']
+        # filtered_ts_confirmed = filtered_ts_confirmed[filtered_ts_confirmed['Country/Region'] != 'China']
+        # filtered_ts_death = filtered_ts_death[filtered_ts_death['Country/Region'] != 'China']
+        # filtered_ts_recovered = filtered_ts_recovered[filtered_ts_recovered['Country/Region'] != 'China']
     
         
     ## Rename columns to prettify hover data
-    temp_deaths_df = filtered_ts_death.rename(columns = {date:'Deaths', 'Lat':'Latitude', 'Long':'Longitude'})
-    temp_recovered_df = filtered_ts_recovered.rename(columns = {date:'Active/Recovered', 'Lat':'Latitude', 'Long':'Longitude'})
-    temp_confirmed_df = filtered_ts_confirmed.rename(columns = {date:'Confirmed', 'Lat':'Latitude', 'Long':'Longitude'})
+    # temp_deaths_df = filtered_ts_death.rename(columns = {date:'Deaths', 'Lat':'Latitude', 'Long':'Longitude'})
+    # temp_recovered_df = filtered_ts_recovered.rename(columns = {date:'Active/Recovered', 'Lat':'Latitude', 'Long':'Longitude'})
+    # temp_confirmed_df = filtered_ts_confirmed.rename(columns = {date:'Confirmed', 'Lat':'Latitude', 'Long':'Longitude'})
     
-    #Some data from JHU appears as -1 unsure why
-    temp_recovered_df[temp_recovered_df['Active/Recovered'] < 0] = 0
-    temp_confirmed_df[temp_confirmed_df['Confirmed'] < 0] = 0
-    temp_deaths_df[temp_deaths_df['Deaths'] < 0] = 0
+    # #Some data from JHU appears as -1 unsure why
+    # temp_recovered_df[temp_recovered_df['Active/Recovered'] < 0] = 0
+    # temp_confirmed_df[temp_confirmed_df['Confirmed'] < 0] = 0
+    # temp_deaths_df[temp_deaths_df['Deaths'] < 0] = 0
     
-    #assumes order of countries from datasets are the same
-    temp_all = temp_confirmed_df[['City/Country', 'Confirmed','Latitude','Longitude']]
-    temp_all.insert(2,'Deaths', temp_deaths_df[['Deaths']])
-    temp_all.insert(2,'Active/Recovered',temp_recovered_df[['Active/Recovered']])
+    # #assumes order of countries from datasets are the same
+    # temp_all = temp_confirmed_df[['City/Country', 'Confirmed','Latitude','Longitude']]
+    # temp_all.insert(2,'Deaths', temp_deaths_df[['Deaths']])
+    # temp_all.insert(2,'Active/Recovered',temp_recovered_df[['Active/Recovered']])
     
     if selected_case == 'Deaths':
-        fig = px.scatter_mapbox(temp_deaths_df, lat="Latitude", lon="Longitude", size='Deaths', size_max=100, hover_name="City/Country")
+        fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", size='deaths', size_max=100, hover_name="country",animation_group='country', animation_frame="date")
         fig.update_traces(hoverinfo='text', marker=dict(sizemin=5, color='Red'))
-    elif selected_case == 'Active/Recovered':
-        fig = px.scatter_mapbox(temp_recovered_df, lat="Latitude", lon="Longitude", size="Active/Recovered",
-                      size_max=100, hover_name="City/Country")
+    elif selected_case == 'Recovered':
+        fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", size="recovered",
+                      size_max=100, hover_name="country",animation_group='country', animation_frame="date")
         fig.update_traces(hoverinfo='text', marker=dict(sizemin=5, color='Green'))
     elif selected_case == 'Confirmed':
-        fig = px.scatter_mapbox(temp_confirmed_df, lat="Latitude", lon="Longitude", size="Confirmed",
-                      size_max=100, hover_name="City/Country")
+        fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", size="confirmed",
+                      size_max=100, hover_name="country",animation_group='country', animation_frame="date")
         fig.update_traces(hoverinfo='text', marker=dict(sizemin=5, color='Blue'))
     else:
-        fig = px.scatter_mapbox(temp_all, lat="Latitude", lon="Longitude", color="Deaths", size="Confirmed",
-                              #color_continuous_scale=px.colors.diverging.Picnic,
-                              size_max=50, hover_name="City/Country",
-                              hover_data=["Confirmed", "Active/Recovered", "Deaths"])
+        fig = px.scatter_mapbox(filtered_df, lat="Latitude", lon="Longitude", size="confirmed", color='deaths',
+                              size_max=50, hover_name="country",
+                              hover_data=["confirmed", "recovered", "deaths"],
+                              animation_group='country',
+                              animation_frame="date"
+                              )
         fig.update_traces(hoverinfo='text', marker=dict(sizemin=2),showlegend=False)
     
     fig.update(
-            layout=dict(title=dict(x=0.5), paper_bgcolor=colors['background'] )
+            layout=dict(title=dict(x=0.5), paper_bgcolor=colors['background'] ), 
         )
     
     fig.update_layout(
         autosize=True,
         height=750,
         #width=1500,
+        legend=dict(font=dict(color=colors['text'])),
         hovermode='closest',
         mapbox=dict(
             accesstoken=mapbox_access_token,
             bearing=0,
             style="dark",
             pitch=0,
-            zoom=zoom
+            zoom=1
         ),
     )
     
@@ -328,9 +334,9 @@ def update_map(selected_nation, selected_case, click, unix_date):
 
 app.layout = Homepage()
 
-# if __name__ == "__main__":
-#     #app.run_server()
-#     app.run_server(debug=True, use_reloader=False)
+if __name__ == "__main__":
+    #app.run_server()
+    app.run_server(debug=True, use_reloader=False)
 
 
     
